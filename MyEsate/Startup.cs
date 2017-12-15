63e608ffaeb041a173a01f373cde9049b9c639ef
Nguyễn Ngọc Lam <ngoclamn@live.com>
2017-12-15 20:16:01 +0700

    Add project files.

---
+++ MyEsate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyEsate.Startup))]
+namespace MyEsate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
